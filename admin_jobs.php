f91a743a5403704c19f36e2baa050bf727c2f3ab
Ravindu Deshitha <ravindudeshitha01@gmail.com>
2025-03-04 21:00:42 +0530

    missing files

--- admin_jobs.php
+++ admin_jobs.php
@@ -10,8 +10,6 @@
 
     <!-- Boxicons CSS -->
 	<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-
-    
 </head>
 <body>
     
--- index.php
+++ index.php
@@ -3,5 +3,4 @@
     
     header("Location: home.php");
 
-
 ?>
